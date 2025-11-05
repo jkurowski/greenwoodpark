2d61663912de717db7cbde49433d715f2b943dc0
Jacek Kurowski <jacek@4dl.pl>
2025-11-05 23:57:54 +0100

    Optymalizacja obrazków

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -33,9 +33,9 @@
     </style>
 
     <!-- Asynchroniczne ładowanie głównego CSS -->
-    <link rel="preload" href="{{ asset('/css/styles.min.css') }}?v21102025" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="preload" href="{{ asset('/css/styles.min.css') }}?v05112025" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript>
-        <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}?v21102025">
+        <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}?v05112025">
     </noscript>
 
     <!-- Asynchroniczne ładowanie CSS dla modali -->
