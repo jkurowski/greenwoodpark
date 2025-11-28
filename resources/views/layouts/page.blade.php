b8801deee2a8c5f584bb19460d01840d3f6adf52
Jacek Kurowski <jacek@4dl.pl>
2025-11-28 23:57:18 +0100

    Podmiana zdjecia na stronie glownej

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -39,7 +39,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
     <!-- Styles -->
-    <link href="{{ asset('/css/styles.min.css') }}?v26112025" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css') }}?v26112025a" rel="stylesheet">
 
     <link rel='preload' as='style' href="{{ asset('/css/styles.min.css') }}">
     @stack('style')
